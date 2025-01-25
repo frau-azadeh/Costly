35bd081e2460355bd6c03fac48f1ca66214fc8a2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-25 04:22:27 -0800

    add ExpenseForm & ExpenseList Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@
 import React from "react";
 import {MainLayout} from "@/components/MainLayout"
 import {ThemeSwitcher} from "@/components/ThemeSwitcher";
+import { ExpenseForm } from "@/components/ExpenseForm";
+import { ExpenseList } from "@/components/ExpenseList";
 
 const HomePage: React.FC = () => {
   return (
     <MainLayout>
       <div className="flex flex-col items-center justify-center min-h-screen">
         <ThemeSwitcher/>
+        <ExpenseForm/>
+        <ExpenseList/>
       </div>
     </MainLayout>
   );
