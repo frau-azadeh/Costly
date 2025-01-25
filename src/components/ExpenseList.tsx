3af8df9e6941dba0e25eb4db5f8cc13fe4a82dde
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-25 04:17:36 -0800

    add list item

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,7 +1,28 @@
+import { useExpenses } from '@/context/ExpenseContext'
 import React from 'react'
 
-export const ExpenseList = () => {
+export const ExpenseList:React.FC = () => {
+  const{expenses}= useExpenses();
+
   return (
-    <div>ExpenseList</div>
+    <div className='max-w-md mx-auto mt-6'>
+      <h2 className='text-2xl font-bold mb-4'>Expenses</h2>
+      {expenses.length === 0 ?(
+        <p> No item added yet.</p>
+      ) : (
+        <ul>
+          {expenses.map((expense)=>(
+            <li className="p-4 border rounded" key={expense.id}>
+              <h3 className='text-lg font-bold'>{expense.title}</h3>
+              <p>Amount: ${expense.amount}</p>
+              <p>Date: {expense.date}</p>
+            </li>
+          ))}
+        </ul>
+      )
+
+      }
+
+    </div>
   )
 }
