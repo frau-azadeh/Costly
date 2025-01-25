807ef3168e3b760bd5f9c1f27d7f50a439914eaf
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-25 04:08:07 -0800

    add form data with react form hook

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,7 +1,46 @@
+import { useExpenses } from '@/context/ExpenseContext';
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
-export const ExpenseForm = () => {
+type FormData = {
+  title: string;
+  amount: number;
+  date: string;
+};
+
+export const ExpenseForm: React.FC = () => {
+  const {register, handleSubmit, reset} = useForm<FormData>();
+  const {addExpense} = useExpenses();
+
+  const onSubmit =  (data: FormData) =>{
+    addExpense(data);
+    reset();
+  }
+
   return (
-    <div>ExpenseForm</div>
+    <div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 max-w-md mx-auto">
+    <input
+        {...register("title", { required: true })}
+        placeholder="Title"
+        className="p-2 border rounded"
+      />
+      <input 
+        {...register("amount", {required: true, valueAsNumber: true})}
+        placeholder='Amount'
+        type='number'
+        className='p-2 border rounded'
+      />
+      <input
+        {...register("date",{required: true})}
+        placeholder='Date'
+        type='date'
+        className='p-2 border rounded'
+      />
+        <button type='submit' className='bg-blue-500 rounded-lg text-white p-2 '>
+          Add Expense
+        </button>
+      </form>
+    </div>
   )
 }
