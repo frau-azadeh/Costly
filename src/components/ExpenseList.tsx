633a385c3b4beb0b44c713762fe8c5a7659d79b8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-26 05:58:40 -0800

    add category in component

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -8,6 +8,7 @@ type FormData = {
   title: string;
   amount: number;
   date: string;
+  category:string;
 };
 
 type ExpenseListProps = {
@@ -15,36 +16,50 @@ type ExpenseListProps = {
 };
 
 export const ExpenseList: React.FC<ExpenseListProps> = ({ setEditingExpense }) => {
-  const { expenses, removeExpense, searchQuery } = useExpenses();
+  const { expenses, removeExpense, searchQuery, filterCategory, setFilterCategory} = useExpenses();
 
   const filterdExpenses = expenses.filter((expense)=>
   expense.title.toLowerCase().includes(searchQuery.toLowerCase()))
-
+  .filter((expense) => (filterCategory ? expense.category === filterCategory : true))
   return (
-    <ul className="flex flex-col gap-4">
-      {filterdExpenses.map((expense) => (
-        <li key={expense.id} className="flex justify-between items-center border-b pb-2">
-          <div>
-            <p>{expense.title}</p>
-            <p>${expense.amount}</p>
-            <p>{expense.date}</p>
-          </div>
-          <div className="flex gap-2">
-            <button
-              onClick={() => setEditingExpense(expense)}
-              className="bg-yellow-500 text-white p-2 rounded"
-            >
-              Edit
-            </button>
-            <button
-              onClick={() => removeExpense(expense.id)}
-              className="bg-red-500 text-white p-2 rounded"
-            >
-              Delete
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="max-w-md max-auto mt-4">
+      <select 
+      value={filterCategory} 
+      onChange={(e) => setFilterCategory(e.target.value)}
+      className="p-2 border rounded w-full mb-4"
+      >
+        <option value="">All Categories</option>
+        <option value="Food">Food</option>
+        <option value="Transportation">Transportation</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Other">Other</option>
+      </select>
+        <ul className="flex flex-col gap-4">
+              {filterdExpenses.map((expense) => (
+                <li key={expense.id} className="flex justify-between items-center border-b pb-2">
+                  <div>
+                    <p>{expense.title}</p>
+                    <p>${expense.amount}</p>
+                    <p>{expense.date}</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <button
+                      onClick={() => setEditingExpense(expense)}
+                      className="bg-yellow-500 text-white p-2 rounded"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => removeExpense(expense.id)}
+                      className="bg-red-500 text-white p-2 rounded"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+        </ul>
+    </div>
+    
   );
 };
